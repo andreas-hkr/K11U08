8dbccdda0f824985c93c7bd66641a1e71ff3a678
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-18 14:46:09 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -19,11 +19,20 @@ public class Main {
         return total;
     }
 
+    static double sigma(double[] arr) {
+        double a = kvadSum(arr);
+        double b = (sum(arr)*sum(arr)) / arr.length;
+
+        return Math.sqrt((a-b)/(arr.length-1));
+    }
+
     public static void main(String[] args) {
         // Skriv en metod sigma som beräknar standardavvikelsen
         // Formel finns i beskrivningen, tipset är att använda
         // våra metoder sum och kvadSum som vi skapade tidigare
 
-
+        double[] arr = {1, 2, 3, 4, 5};
+        double res = sigma(arr);
+        System.out.println("Resultat: " + res);
     }
 }
